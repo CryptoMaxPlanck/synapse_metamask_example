1fe9f97369b5988f0cb25290388d58996dbfbfe3
Max Planck <maxplanck.crypto@gmail.com>
2022-07-19 11:40:06 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <button id="connectButton">Connect</button>
+        <button id="signApprove">Sign Approval</button>
+        <button id="signBridge">Sign Bridge</button>
+    </div>
+  );
+}
+
+export default App;
